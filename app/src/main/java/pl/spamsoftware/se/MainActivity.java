0bd09788a00ce87c723c3b20b3b5ce43ebdb97a5
Kyroaku <volffikk@gmail.com>
2016-07-19 08:19:47 +0200

    Initial commit

---
+++ app/src/main/java/pl/spamsoftware/se/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.spamsoftware.se;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
