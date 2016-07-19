628dac763047291184d1c6a751988042c4a6fa37
Kyroaku <volffikk@gmail.com>
2016-07-19 08:27:33 +0200

    Init

--- app/src/main/java/pl/spamsoftware/se/MainActivity.java
+++ app/src/main/java/pl/spamsoftware/se/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+		//init
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
